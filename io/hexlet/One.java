0dfd4c306fae836772bf97ad1e76b1d52a4c2af0
Gilberto <zzzest@gmail.com>
2023-08-07 12:30:40 +0300

    add java files

---
+++ io/hexlet/One.java
@@ -0,0 +1,7 @@
+package io.hexlet;
+
+public class One {
+    public static void main(String[] args) {
+        Two.greeting();
+    }
+}
---
+++ io/hexlet/Two.java
@@ -0,0 +1,7 @@
+package io.hexlet;
+
+public class Two {
+    public static void greeting() {
+        System.out.println("Java for Brave");
+    }
+}
